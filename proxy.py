be920711b59924c8f31dce7ed69528fb4d894e40
Mahmoud-Al-Hajj <mah06.hajj@gmail.com>
2026-04-11 21:19:59 +0300

    Implement multithreading for client handling in proxy server

--- proxy.py
+++ proxy.py
@@ -1,6 +1,7 @@
 # proxy.py - Entry point. Starts the proxy server.
 
 import socket
+import threading
 from config import PROXY_HOST, PROXY_PORT
 from logger import log
 from handler import handle_client
@@ -18,12 +19,18 @@ def start_proxy():
     try:
         while True:
             client_socket, client_address = server_socket.accept()
-            handle_client(client_socket, client_address)
+
+            # Spawn a new thread for this client so we can immediately
+            # go back and accept the next one without waiting.
+            thread = threading.Thread(
+                target=handle_client,
+                args=(client_socket, client_address)
+            )
+            thread.daemon = True   # thread dies immediately intead of waiting to finish.
+            thread.start()
+# Client A connects → thread spawns for A → proxy immediately accepts Client B → both handled in parallel
+
     except KeyboardInterrupt:
         log("Proxy stopped.")
     finally:
         server_socket.close()
-
-
-if __name__ == '__main__':
-    start_proxy()
